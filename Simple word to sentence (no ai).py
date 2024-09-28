489599dd59370d7bbbcdc306b3523961301593eb
Sreyan <sreyanmithra@gmail.com>
2024-09-28 17:22:05 +0530

    Add files via upload

---
+++ Simple word to sentence (no ai).py
@@ -0,0 +1,13 @@
+name = str(input("Enter your name: "))
+
+age = int(input("\nEnter your age: "))
+
+studies = str(input("\nWhat are you studying: "))
+
+hobbies = str(input("\nWhat are your hobbies: "))
+
+goal = str(input("\nWhat is your goal: "))
+
+print(f"\nI am {name} and my age is {age} and I am studying in {studies} and my hobbies are {hobbies},\nmy goal is {goal}.")
+
+print("\n :)")
